c0b4e295be7f3efe384f2a94d0db2d28eff75644
morvarid61 <marij6191@gmail.com>
2025-05-13 22:40:47 +0330

    img4

--- assets/main-B_w0fXvO.js
+++ assets/main-B_w0fXvO.js
@@ -1,4 +1,4 @@
-import"./tailwind-DYkSoCjc.js";let c=(...o)=>{o.forEach(e=>{document.querySelector("#root").insertAdjacentHTML("beforeend",e)})},i=` <div class="flax justify-center items-center h-screen ">
+import"./tailwind-DYkSoCjc.js";let c=(...n)=>{n.forEach(e=>{document.querySelector("#root").insertAdjacentHTML("beforeend",e)})},i=` <div class="flax justify-center items-center h-screen ">
         <div>
           <h1 class="sm:text-[60px] md:text-[80px]  sm:w-full lg:w-full  text-white text-center p-4">
             السلام علیک یا ابا عبدالله
@@ -8,7 +8,7 @@ import"./tailwind-DYkSoCjc.js";let c=(...o)=>{o.forEach(e=>{document.querySelect
        <div>
           <img
             class="w-[250px] m-auto pb-4"
-            src="./src/image/Logo Mookeb.png"
+            src="./src/image/logo.png"
             alt=""/>
         </div>
     
@@ -28,4 +28,4 @@ import"./tailwind-DYkSoCjc.js";let c=(...o)=>{o.forEach(e=>{document.querySelect
             </div>
           </div>
         </div>
-      </div>`;const x=()=>{const o=new Date("2025-07-11T00:00:00"),e=document.getElementById("countdown"),n=()=>{const t=o-new Date;if(t<=0){e.textContent="00:00:00:00",clearInterval(s),window.location.href="./test.html";return}const a=Math.floor(t/(1e3*60*60*24)),d=Math.floor(t%(1e3*60*60*24)/(1e3*60*60)),r=Math.floor(t%(1e3*60*60)/(1e3*60)),l=Math.floor(t%(1e3*60)/1e3);e.textContent=String(a).padStart(2,"0")+":"+String(d).padStart(2,"0")+":"+String(r).padStart(2,"0")+":"+String(l).padStart(2,"0")},s=setInterval(n,1e3);n()};c(i);x();
+      </div>`;const x=()=>{const n=new Date("2025-07-11T00:00:00"),e=document.getElementById("countdown"),o=()=>{const t=n-new Date;if(t<=0){e.textContent="00:00:00:00",clearInterval(s),window.location.href="./test.html";return}const a=Math.floor(t/(1e3*60*60*24)),d=Math.floor(t%(1e3*60*60*24)/(1e3*60*60)),l=Math.floor(t%(1e3*60*60)/(1e3*60)),r=Math.floor(t%(1e3*60)/1e3);e.textContent=String(a).padStart(2,"0")+":"+String(d).padStart(2,"0")+":"+String(l).padStart(2,"0")+":"+String(r).padStart(2,"0")},s=setInterval(o,1e3);o()};c(i);x();
